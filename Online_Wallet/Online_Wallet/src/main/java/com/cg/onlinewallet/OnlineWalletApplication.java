9b9e81466d7dc22ee75148e56c25270178fb1efb
Vinay Kumar Singh <vinay.singh_cs16@gla.ac.in>
2020-05-23 17:36:33 +0530

    Update OnlineWalletApplication.java

--- Online_Wallet/Online_Wallet/src/main/java/com/cg/onlinewallet/OnlineWalletApplication.java
+++ Online_Wallet/Online_Wallet/src/main/java/com/cg/onlinewallet/OnlineWalletApplication.java
@@ -36,9 +36,9 @@ public class OnlineWalletApplication implements CommandLineRunner {
 		em.persist(wa1);
 		em.persist(wa2);
 		em.persist(wa3);
-		WalletUser user1=new WalletUser("Vinay", "123", "876543", "email@abc.com",type.user,  wa1);
-		WalletUser user2=new WalletUser("kumar", "123", "876543", "email@abc.com",type.user,  wa2);
-		WalletUser user3=new WalletUser("singh", "1234", "8765432", "email@abcd.com",type.user,  wa3);
+		WalletUser user1=new WalletUser("Vinay", "123", "876543", "email@ab.com",type.user,  wa1);
+		WalletUser user2=new WalletUser("kumar", "1234", "8765432", "email@abc.com",type.user,  wa2);
+		WalletUser user3=new WalletUser("singh", "12345", "87654321", "email@abcd.com",type.user,  wa3);
 		em.persist(user1);
 		em.persist(user2);
 		em.persist(user3);
